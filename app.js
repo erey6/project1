e85fdd555a1f0fc971c14cfe49053c665bf9be2e
Eric Reyes <ericreyes@gmail.com>
2021-10-02 14:05:40 -0500

    wireframe and zipcode js input set up

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
+
 $(() => {
+    $('form').on('submit', (event) => {
+        event.preventDefault();
+        const zipCode = $('input[type="text"]').val();
+        $('form').trigger('reset');
+        downloadData(zipCode)
+    })
 
-
-    $.ajax({
-        url: "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Tree%20Planting%20Request&status=Completed&zip_code=60614&$order=created_date%20DESC",
-        type: "GET",
-        data: {
-            "$limit": 5000,
+    const renderData = (data) => {
+        for (const request of data) {
+            console.log(request.street_address);
+            console.log(request.created_date);
+            console.log(request.community_area);
+            console.log(request.zip_code)
         }
-    }).then(
-        (data) => {
-            let counter = 0
-            let myDate = new Date(2020, 01, 01)
-            for (const request of data) {
+    }
 
-                if (request.status === "Completed") {
-                    
-                    let completedDate = new Date(request.closed_date)
-                    if (completedDate > myDate && request.zip_code === "60634") {
-                    counter++
-                    console.log(myDate)
-                    console.log("completed", completedDate)
-                    console.log("last modified", request.last_modified_date );
-                    // console.log("created", request.created_date);
-                    // console.log("closed", request.closed_date);
-                    // console.log("community_area", request.community_area);
-                    console.log(request.zip_code)
-                }
-                console.log("counter", counter);
-                }
+    const downloadData = (zipCode) => {
+
+        console.log(zipCode)
+        $.ajax({
+            url: `https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Tree%20Planting%20Request&duplicate=false&status=Open&zip_code=${zipCode}&$order=created_date%20DESC`,
+            type: "GET",
+            data: {
+                "$limit": 1000,
             }
-        },
-        () => {
-            console.log('bad request');
-        }
-    )
+        }).then(
+            (data) => {
+
+                renderData(data)
+            },
+            () => {
+                console.log('bad request');
+            }
+        )
+
+    }
 })
