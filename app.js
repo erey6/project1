484dff28aa76bb6bb6dfa974304539f0accc0395
Eric Reyes <ericreyes@gmail.com>
2021-10-02 19:37:27 -0500

    added community areas

--- app.js
+++ app.js
@@ -1,11 +1,90 @@
+const comAreas = ["",
+"Rogers Park",
+"West Ridge",
+"Uptown",
+"Lincoln Square",
+"North Center",
+"Lake View",
+"Lincoln Park",
+"Near North Side",
+"Edison Park",
+"Norwood Park",
+"Jefferson Park",
+"Forest Glen",
+"North Park",
+"Albany Park",	
+"Portage Park",
+"Irving Park",
+"Dunning",
+"Montclare",
+"Belmont Cragin",
+"Hermosa",
+"Avondale",
+"Logan Square",
+"Humboldt Park",
+"West Town",
+"Austin",
+"West Garfield Park",
+"East Garfield Park",
+"Near West Side",
+"North Lawndale",
+"South Lawndale",
+"Lower West Side",
+"(The) Loop",
+"Near South Side",
+"Armour Square",
+"Douglas",
+"Oakland",
+"Fuller Park",
+"Grand Boulevard",
+"Kenwood",
+"Washington Park",
+"Hyde Park",
+"Woodlawn",
+"South Shore",
+"Chatham",
+"Avalon Park",
+"South Chicago",
+"Burnside",
+"Calumet Heights",
+"Roseland",
+"Pullman",
+"South Deering",
+"East Side",
+"West Pullman",
+"Riverdale",
+"Hegewisch",
+"Garfield Ridge",
+"Archer Heights",
+"Brighton Park",
+"McKinley Park",
+"Bridgeport",
+"New City",
+"West Elsdon",
+"Gage Park",
+"Clearing",
+"West Lawn",
+"Chicago Lawn",
+"West Englewood",
+"Englewood",
+"Greater Grand Crossing",
+"Ashburn",
+"Auburn Gresham",
+"Beverly",
+"Washington Heights",
+"Mount Greenwood",
+"Morgan Park",
+"O'Hare",
+"Edgewater"]
+
 const readableDate = (date) => {
 
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    // const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     const d = new Date(date)
-    const monthIndex = d.getMonth();
+    const month = d.getMonth() + 1;
     const day = d.getDate();
     const year = d.getFullYear();
-    return `${months[monthIndex]} ${day}, ${year}`
+    return `${month}/${day}/${year}`
 
 }
 
@@ -27,7 +106,7 @@ $(() => {
             const $div = $('<div>').addClass('row-result')
             $div.append($('<p>').text(`${request.street_address}`))
             $div.append($('<p>').text(`${dateString}`))
-            // console.log(request.created_date);
+            $div.append($('<p>').text(`${comAreas[request.community_area]}`))
             // console.log(request.community_area);
             // console.log(request.zip_code);
             $('.results').append($div)
